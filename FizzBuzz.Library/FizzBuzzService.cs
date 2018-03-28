ff4c709cff977807910c1d0180862e4ff9d08bde
Keaton Wineman <Keatonwineman@gmail.com>
2018-03-28 19:02:05 -0400

    refactoring

--- FizzBuzz.Library/FizzBuzzService.cs
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -8,11 +8,11 @@ namespace FizzBuzz.Library
         public String Print(int v)
         {
             String answer = "";
-            if(v % 3 == 0)
+            if(IsDivisibleBy3(v))
             {
                 answer="Fizz";
             }
-            if (v % 5 == 0)
+            if (IsDivisibleBy5(v))
             {
                 answer+="Buzz";
             }
@@ -21,6 +21,14 @@ namespace FizzBuzz.Library
                 return v.ToString();
             }
             return answer;
+        }
+        private bool IsDivisibleBy3(int number)
+        {
+            return (number % 3 == 0);
+        }
+        private bool IsDivisibleBy5(int number)
+        {
+            return (number % 5 == 0);
         }
     }
 }
